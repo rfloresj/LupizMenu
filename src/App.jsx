47d6ef58d6691de90671ef8a1067844c588f3c74
rflores <104962825+rfloresj@users.noreply.github.com>
2023-11-13 17:05:33 -0600

    WIP:Border

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
-import lupizLogo from "./images/Lupiz.png";
+import lupisLogo from "./images/Lupis.png";
+import borderLupis from "./images/Border_Lupis.png";
 
 function App() {
   return (
     <>
+      <img id="main-border" src={borderLupis} alt="border" />
       <header>
         <div className="lupis-container">
-          <img className="lupis-logo" src={lupizLogo} alt="lupis-logo" />
+          <img className="lupis-logo" src={lupisLogo} alt="lupis-logo" />
         </div>
       </header>
     </>
